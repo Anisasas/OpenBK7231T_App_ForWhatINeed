69548a6d21548c247238a0601878d61bb0086d50
Anisasas <168284615+Anisasas@users.noreply.github.com>
2026-05-23 10:23:31 +0300

    Update obk_config.h

--- src/obk_config.h
+++ src/obk_config.h
@@ -735,9 +735,11 @@
 #define ENABLE_HTTP_DGR							1
 #endif
 
+// Force this to 1 globally so your BL602 custom target always has it
+#define ENABLE_BL_SHARED						1
+
 // if power metering chip is enabled, also enable backend for that
 #if ENABLE_DRIVER_BL0937 || ENABLE_DRIVER_BL0942 || ENABLE_DRIVER_BL0942SPI || ENABLE_DRIVER_CSE7766
-#define ENABLE_BL_SHARED						1
 // allow use two BL0942 on two ports  +940 bytes
 // #define ENABLE_BL_TWIN						1
 // allow moving average energy calculation +180 bytes
